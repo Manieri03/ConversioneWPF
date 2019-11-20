a0ace980477c635d35233c20cae1c85828d8401f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-20 13:17:17 +0100

    Inserite le due conversioni

--- ConversioneWPF/MainWindow.xaml.cs
+++ ConversioneWPF/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace ConversioneWPF
                         }
                         else if (Valorestring[i] != '0')
                         {
-                            throw new Exception("Non è un numero valido");
+                            throw new Exception("Non è un numero valido ");
                         }
                         exp++;
                     }
